48a0007f9b3767e2924000a000c4ea7cec67acbe
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-05-21 17:03:44 -0500

    creando una lista de usuarios con List y Arrays.asList y luego mostrandola en index html usando each de thymeleaf

--- src/main/java/com/VMgarciaRivera/flight_management/ControladorInicio.java
+++ src/main/java/com/VMgarciaRivera/flight_management/ControladorInicio.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.VMgarciaRivera.flight_management.modelo.Usuario;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Slf4j
 public class ControladorInicio {
@@ -28,6 +32,20 @@ public class ControladorInicio {
         usuario.setClave("123456");
         usuario.setNombre("Victor Manuel Garcia Rivera");
         modelo.addAttribute("usuario", usuario);
+        Usuario u2 = new Usuario();
+        u2.setCedula("983u98237");
+        u2.setEmail("cnaindioqndi");
+        u2.setClave("qweq3e3qwd");
+        u2.setNombre("Vjose ioajsoisaa");
+        Usuario u3 = new Usuario();
+        u3.setCedula("asodjaijoiw");
+        u3.setEmail("lansdlianld");
+        u3.setClave("sadmaoisdjaos8");
+        u3.setNombre("mhvhhtfy");
+
+        List<Usuario> listaUsuarios = Arrays.asList(u2, u3);
+        modelo.addAttribute("listaUsuarios", listaUsuarios);
+
         log.info("Accediendo a la pagina de inicio");
         return "index";
     }
