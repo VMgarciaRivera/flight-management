09e411c4152b05ee28eacd54ce7e8ed541bccc25
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-05-25 16:40:49 -0500

    feat: agregar funcionalidades de edición y eliminación de usuarios, incluyendo validación de datos en el formulario

--- src/main/java/com/VMgarciaRivera/flight_management/controladores/ControladorInicio.java
+++ src/main/java/com/VMgarciaRivera/flight_management/controladores/ControladorInicio.java
@@ -1,14 +1,16 @@
 package com.VMgarciaRivera.flight_management.controladores;
 
 import com.VMgarciaRivera.flight_management.servicio.IUsuarioServicio;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.VMgarciaRivera.flight_management.dao.IUsuarioCrud;
 import com.VMgarciaRivera.flight_management.modelo.Usuario;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -29,13 +31,30 @@ public class ControladorInicio {
     }
 
     @GetMapping("/agregar")
-    public String agregar(Usuario usuario) {
+    public String agregar(@ModelAttribute("usuario") Usuario usuario) {
         return "modificar";
     }
 
     @PostMapping("/guardar")
-    public String guardar (Usuario usuario) {
+    public String guardar (@Valid Usuario usuario, Errors errores) {
+        if (errores.hasErrors()) {
+            return "modificar";
+        }
         usuarioServicio.guardar(usuario);
+        return "redirect:/";
+    }
+
+    @GetMapping("/editar/{cedula}")
+    public String editar(Usuario usuario, Model modelo) {
+        log.info("ejecutando control de editar MVC");
+        usuario = usuarioServicio.encontrarUsuario(usuario);
+        modelo.addAttribute("usuario", usuario);
+        return "modificar";
+    }
+
+    @GetMapping("/eliminar/{cedula}")
+    public String eliminar(Usuario usuario) {
+        usuarioServicio.eliminar(usuario);
         return "redirect:/";
     }
 
--- src/main/java/com/VMgarciaRivera/flight_management/modelo/Usuario.java
+++ src/main/java/com/VMgarciaRivera/flight_management/modelo/Usuario.java
@@ -1,9 +1,10 @@
 package com.VMgarciaRivera.flight_management.modelo;
 
 import java.io.Serializable;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -12,8 +13,15 @@ import lombok.Data;
 public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @Basic(optional = false)
+    @NotBlank
+    @Column(name = "cedula", nullable = false, length = 15)
     private String cedula;
+    @NotBlank
     private String clave;
+    @NotBlank
     private String nombre;
+    @NotBlank
+    @Email
     private String email;
 }
