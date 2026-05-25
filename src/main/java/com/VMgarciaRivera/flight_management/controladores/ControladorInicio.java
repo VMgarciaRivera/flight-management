95c9428cb2916cbad419e160d4a845a39013aebb
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-05-25 14:42:46 -0500

    feat: agregar funcionalidad para listar y agregar usuarios en el controlador de inicio, incluyendo una nueva vista para modificar datos del usuario

--- src/main/java/com/VMgarciaRivera/flight_management/controladores/ControladorInicio.java
+++ src/main/java/com/VMgarciaRivera/flight_management/controladores/ControladorInicio.java
@@ -1,23 +1,42 @@
 package com.VMgarciaRivera.flight_management.controladores;
 
+import com.VMgarciaRivera.flight_management.servicio.IUsuarioServicio;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.VMgarciaRivera.flight_management.dao.IUsuarioCrud;
 import com.VMgarciaRivera.flight_management.modelo.Usuario;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
 
+    @Autowired
+    IUsuarioServicio usuarioServicio;
+
     @GetMapping("/")
     public String inicio(Model modelo) {
+        List<Usuario> listaUsuarios = (List<Usuario>) usuarioServicio.listarUsuarios();
+        modelo.addAttribute("listaUsuarios", listaUsuarios);
         log.info("ejecutando control de inicio MVC");
         return "index";
     }
+
+    @GetMapping("/agregar")
+    public String agregar(Usuario usuario) {
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar (Usuario usuario) {
+        usuarioServicio.guardar(usuario);
+        return "redirect:/";
+    }
+
 }
