74c9c99e90eb54e248e18f2358c99d2967336fd1
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-05-25 14:41:16 -0500

    feat: agregar entidad Usuario con anotaciones JPA y serialización

--- src/main/java/com/VMgarciaRivera/flight_management/modelo/Usuario.java
+++ src/main/java/com/VMgarciaRivera/flight_management/modelo/Usuario.java
@@ -1,9 +1,17 @@
 package com.VMgarciaRivera.flight_management.modelo;
 
+import java.io.Serializable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "usuarios")
 @Data
-public class Usuario {
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
     private String cedula;
     private String clave;
     private String nombre;
