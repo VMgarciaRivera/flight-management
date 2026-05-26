24c73a96017171b5f2b47ffe616f3c57476b7efa
VMgarciaRivera <vgarciar@unicartagena.edu.co>
2026-05-26 15:20:31 -0500

    feat: modelo de vuelos creado, se evito el uso de @Data por buenas practicas y se cambio por getter y setter de lombok

---
+++ src/main/java/com/VMgarciaRivera/flight_management/modelo/Vuelo.java
@@ -0,0 +1,77 @@
+package com.VMgarciaRivera.flight_management.modelo;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "vuelos")
+@Getter
+@Setter
+public class Vuelo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, columnDefinition = "UUID", nullable = false)
+    private UUID id;
+
+    @NotNull
+    @Column(name = "fecha_compra", nullable = false)
+    private ZonedDateTime fechaCompra;
+
+    @NotNull
+    @Column(name = "fecha_salida", nullable = false)
+    private ZonedDateTime fechaSalida;
+
+    @NotNull
+    @Column(name = "fecha_llegada", nullable = false)
+    private ZonedDateTime fechaLlegada;
+
+    @NotBlank
+    @Column(name = "agencia_viajes")
+    private String agenciaViajes;
+
+    @NotBlank
+    private String aerolinea;
+
+    @NotBlank
+    private String numero;
+
+    @NotBlank
+    private String estado;
+
+    @NotNull
+    @Positive
+    @Column(precision = 14, scale = 2)
+    private BigDecimal valor;
+
+    @NotBlank
+    private String cliente;
+
+    @NotBlank
+    private String puesto;
+
+    @NotBlank
+    private String avion;
+
+    @NotBlank
+    @Column(name = "aeropuerto_salida")
+    private String aeropuertoSalida;
+
+    @NotBlank
+    @Column(name = "aeropuerto_llegada")
+    private String aeropuertoLlegada;
+
+    @NotBlank
+    private String piloto;
+}
